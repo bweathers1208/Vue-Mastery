2c3ea2c79798f5e3b231345df64797ab42058066
Bri Weathers <55861115+bweathers1208@users.noreply.github.com>
2020-06-22 10:53:14 -0500

    Update main.js per Lesson 5 - Event Handling

--- main.js
+++ main.js
@@ -1,7 +1,36 @@
 var app = new Vue({
     el: '#app',
     data: {
-        product: 'Socks'
+        product: 'Socks',
+        description: 'Unisex, soft socks',
+        image: './src/assets/js/vmSocks-green.jpg',
+        link: '#',
+        inventory: 6,
+        onSale: false,
+        details: ["80% cotton", "20% polyester", "Gender-neutral"],
+        variants: [
+            {
+                variantId: 2234,
+                variantImage: './src/assets/js/vmSocks-green.jpg',
+                variantColor: "green"
+            },
+            {
+                variantId: 2235,
+                variantImage: './src/assets/js/vmSocks-blue.jpg',
+                variantColor: "blue"
+            }
+        ],
+        sizes: ["small", "medium", "large"],
+        cart: 0
+        },
+        methods: {
+            addToCart() {
+                this.cart += 1
+            },
+            updateProduct(variantImage) {
+                this.image = variantImage
+            }
+
     }
 
 })
