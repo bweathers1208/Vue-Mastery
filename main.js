510f460db81e677084007a21f3bc4c9f2dd7b2af
Bri Weathers <55861115+bweathers1208@users.noreply.github.com>
2020-06-23 11:08:05 -0500

    Update main.js per lesson 7

--- main.js
+++ main.js
@@ -1,28 +1,33 @@
 var app = new Vue({
     el: '#app',
     data: {
+        brand: 'Vue Mastery',
         product: 'Socks',
         description: 'Unisex, soft socks',
         image: './src/assets/js/vmSocks-green.jpg',
         link: '#',
         inventory: 6,
-        onSale: false,
+        onSale: true,
+        inStock: true,
         details: ["80% cotton", "20% polyester", "Gender-neutral"],
+        
         variants: [
             {
                 variantId: 2234,
                 variantImage: './src/assets/js/vmSocks-green.jpg',
-                variantColor: "green"
+                variantColor: "#50C878"
             },
             {
                 variantId: 2235,
                 variantImage: './src/assets/js/vmSocks-blue.jpg',
-                variantColor: "blue"
+                variantColor: "#1c4966"
             }
         ],
+
         sizes: ["small", "medium", "large"],
         cart: 0
         },
+
         methods: {
             addToCart() {
                 this.cart += 1
@@ -31,6 +36,14 @@ var app = new Vue({
                 this.image = variantImage
             }
 
+    }, 
+    computed: {
+        title() {
+            return this.brand + ' ' + this.product 
+        },
+        saleItem() {
+            return this.brand + ' ' + this.product
+        }
     }
 
 })
